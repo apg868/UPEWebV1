541cd0c10fc27dda8bc1bf89932871b2e74e518f
apg868 <123202954+apg868@users.noreply.github.com>
2023-10-20 04:15:01 -0400

    testing continuted

--- upe_web/src/components/NavBar/BaseNav.js
+++ upe_web/src/components/NavBar/BaseNav.js
@@ -6,7 +6,7 @@ import logomain from './../../assets_folder/logoWHITE.png';;
 function BaseNav() {
   return (
     <Flex position="sticky" top={0} left={0} right={0} bg={['primaryRed', 'secondaryRed', 'primaryGray', 'secondaryRed', 'primaryRed']} w='100%' h="10vh" zIndex={1}>
-      <HStack spacing={['7', '8', '9','100']} paddingLeft={["0vw", "20vw", "25vw", "40vw"]}>
+      <HStack spacing={['7', '10', '7em', '8em', '9em']} paddingLeft={["0vw", "15em", "10em", "15em", "21em"]}>
         <Text fontFamily="primaryFont" fontWeight="normal" fontSize={['md', 'xl', '2xl', '3xl', '4xl']} pt={1}> hello </Text>
         <Text fontFamily="primaryFont" fontWeight="normal" fontSize={['md', 'xl', '2xl', '3xl', '4xl']} pt={1}> cello </Text>
         <Text fontFamily="primaryFont" fontWeight="normal" fontSize={['md', 'xl', '2xl', '3xl', '4xl']} pt={1}> fellow</Text>

0242738522d4c1d360763e6885d25bf8f8395a49
apg868 <123202954+apg868@users.noreply.github.com>
2023-10-21 02:58:56 -0400

    card spacing update. Mobile responsiveness TBD

--- upe_web/src/components/MainPage/CardSect/CardFrame.js
+++ upe_web/src/components/MainPage/CardSect/CardFrame.js
@@ -7,15 +7,16 @@ function CardFrame(props) {
     const image = props.image
 
     return(
-        <Box bg="primaryGray" width={{"md":"15em", "lg":"25em"}} height={{"md":"30em","lg":"35em"}} borderRadius="0.5em"
-        paddingTop={{"md":".75em","lg":"1em"}}
-        paddingLeft={{"md":".75em","lg":"2em"}}
-        paddingRight={{"md":".75em","lg":"2em"}}
+        <Box bg="primaryGray" width={{"md":"21em", "lg":"26em"}} height={{"md":"30em","lg":"35em"}} borderRadius="0.5em"
         >
-            <VStack justifyContent="center">
-                <Image src={image} width ={{"md":"14em", "lg":"24em"}} height={{"md":"10em","lg":"20em"}} borderRadius="0.5em"/>
+            <VStack justifyContent="center" paddingTop={{"md":".75em","lg":"1em"}}>
+                <Image src={image} width ={{"md":"19em", "lg":"24em"}} height={{"md":"10em","lg":"20em"}} borderRadius="0.5em"/>
+                <Box
+        paddingLeft={{"md":".75em","lg":"2em"}}
+        paddingRight={{"md":".75em","lg":"2em"}}>
                 <Text fontFamily="secondaryFont" fontWeight="medium" maxWidth="100%" fontSize={{"base":"lg","md":"xl","lg":"2xl"}}>  {props.title} </Text>
                 <Text fontFamily="secondaryFont" fontWeight="normal" maxWidth="100%"fontSize={{"base":"md","md":"lg","lg":"xl"}}>  {props.description} </Text>
+                </Box>
             </VStack>
         </Box>
 

4d7872e77dcd720d33bf08962bb7434007ad2ae0
apg868 <123202954+apg868@users.noreply.github.com>
2023-10-20 22:54:54 -0400

    navbar interactivity

--- upe_web/src/components/NavBar/BaseNav.js
+++ upe_web/src/components/NavBar/BaseNav.js
@@ -1,28 +1,38 @@
 import { Flex, HStack, Image, Text } from "@chakra-ui/react";
 import { Link } from 'react-router-dom';
 import logoRED from './../../assets_folder/logoRED.png';
-import { createBreakpoints } from '@chakra-ui/theme-tools';
-
-
-const breakpoints = createBreakpoints({
-  sm: '360px',
-  md: '600px',
-  lg: '1079px',
-  xl: '1440px',
-});
-
 
 function BaseNav() {
   return (
     <Flex position="sticky" top={0} left={0} right={0} bg={{"md": "primaryGray"}} w='100%' h="9vh" zIndex={1}>
       <HStack spacing={{"md":"3em","lg":"15em"}}>
         <Image src={logoRED} boxSize="9vh" borderRadius="4em"/>
-      <HStack spacing={{"md": "6em", "lg": "7em"}} paddingLeft={{"md": "3em", "lg": "4em"}}>
-        <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1}> Home </Text>
-        <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1}> Members </Text>
-        <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1}> Intern Projects </Text>
-        <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1}> About Us </Text>
-      </HStack>
+        <HStack spacing={{"md": "6em", "lg": "7em"}} paddingLeft={{"md": "3em", "lg": "4em"}}>
+          <Link to="/">
+            <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1} 
+                  _hover={{ color: "primaryRed"}}>
+              Home
+            </Text>
+          </Link>
+          <Link to="/members">
+            <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1} 
+                  _hover={{ color: "primaryRed"}}>
+              Members
+            </Text>
+          </Link>
+          <Link to="/intern-projects">
+            <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1} 
+                  _hover={{ color: "primaryRed"}}>
+              Intern Projects
+            </Text>
+          </Link>
+          <Link to="/about-us">
+            <Text fontFamily="primaryFont" fontWeight="thin" fontSize={{"md": "xl","lg":"2xl"}} pt={1} 
+                  _hover={{ color: "primaryRed"}}>
+              About Us
+            </Text>
+          </Link>
+        </HStack>
       </HStack>
     </Flex>
   );

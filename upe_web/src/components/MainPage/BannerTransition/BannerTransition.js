6c4922682bcdd3360e4e97b11855eef4ef4e70d4
apg868 <123202954+apg868@users.noreply.github.com>
2023-10-21 20:45:17 -0400

    CTA fully responsive

--- upe_web/src/components/MainPage/BannerTransition/BannerTransition.js
+++ upe_web/src/components/MainPage/BannerTransition/BannerTransition.js
@@ -2,11 +2,32 @@ import { useBreakpointValue, Flex, Text, Box, Image, HStack, VStack } from "@cha
 
 function BannerTransition() {
     return (
-        <Flex bg="primaryRed" w="90vw" marginLeft="5vw" marginRight="5vw" position="relative" transform="translateY(-50%)"
-            borderRadius="2em" justifyContent="center" alignItems="center">
-            <Text fontSize={{ "base": "lg", "md": "2xl", "lg": "4xl" }}
-                fontFamily="secondaryFont" color="white" fontWeight="normal">
-                What UPE Has to Offer</Text>
+        <Flex
+            position="relative"
+            top="0"
+            right="0"
+            bottom="0"
+            left="0"
+            transform="translateY(-50%)"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Flex
+                bg="primaryRed"
+                w={{"base":"45em","md":"60em","lg":"90em"}}
+                borderRadius="2em"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Text
+                    fontSize={{ "base": "lg", "md": "2xl", "lg": "4xl" }}
+                    fontFamily="secondaryFont"
+                    color="white"
+                    fontWeight="normal"
+                >
+                    What UPE Has to Offer
+                </Text>
+            </Flex>
         </Flex>
     )
 }
--- upe_web/src/components/MainPage/CTA/CTAOrg/CTAMobile.js
+++ upe_web/src/components/MainPage/CTA/CTAOrg/CTAMobile.js
@@ -1,13 +1,56 @@
-import { HStack, VStack, Flex, Text } from "@chakra-ui/react";
+import { HStack, VStack, Flex, Text, Button} from "@chakra-ui/react";
 
 
 function CTAMobile(){
     return(
-        <>
-        <Flex bg="primaryRed" width="90vw" height="30vh">
-        HELLO
+        <Flex
+        position="relative"
+        top="0"
+        right="0"
+        bottom="0"
+        left="0"
+        justifyContent="center"
+        alignItems="center">
+        <Flex bg="primaryRed" width="20em" height="25em" marginTop="5em"
+         justifyContent="center" borderRadius="2em" p="1.5em" align="center" alignSelf="center">
+            <VStack spacing="5em">
+                <Text color="white" fontSize="2xl"
+                fontFamily="secondaryFont" fontWeight="medium"> Want to Join? </Text>
+                <VStack spacing="2.5em">
+                    <a href="https://docs.google.com/forms/d/1HjThY70Ucu2ne6P3kPqFxn31WBTdu9P12SGX5YFTTE8/closedform" target="_blank" rel="noopener noreferrer">
+                        <Button
+                            bg="white"
+                            borderRadius="2em"
+                            fontFamily="secondaryFont"
+                            fontWeight="normal"
+                            color="primaryRed"
+                            w="13em"
+                            fontSize="md"
+                        >
+                            Application
+                        </Button>
+                    </a>
+                    <a href="https://www.signupgenius.com/go/10C084EA5AA29A2F4CF8-upeinterview#" target="_blank" rel="noopener noreferrer">
+                        <Button
+                            bg="white"
+
+                            borderRadius="2em"
+                            fontFamily="secondaryFont"
+                            fontWeight="normal"
+                            color="primaryRed"
+                            w="13em"
+                            fontSize="md"
+                        >
+                            Interview Sign-up
+                        </Button>
+                    </a>
+                    </VStack>
+                <Flex w="22em" bg="white" height="2em" justifyContent="center" borderRadius="0.5em" Align="center"
+                fontFamily="secondaryFont" fontSize="sm" marginTop={{"md":"1em", "lg":"2em"}}
+                fontWeight="light"> Our applications open every fall and spring semester</Flex>
+            </VStack>
         </Flex>
-        </>
+        </Flex>
     )
 }
 

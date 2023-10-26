e4346cb95b7006df9a9d0bff1a04f9c8adb15134
ajamias <ajamias@bu.edu>
2023-10-26 15:09:02 -0400

    members list is now scalable for desktop

--- src/components/MembersPage/MembersGrid/MemberFrame.js
+++ src/components/MembersPage/MembersGrid/MemberFrame.js
@@ -1,11 +1,12 @@
-import { useBreakpointValue, Flex, Text, Box, Image, HStack, VStack } from "@chakra-ui/react";
+import { Flex, Text, Image, VStack } from "@chakra-ui/react";
 
 function MemberFrame(props){
-    const image = props.image
     const name= props.name
     const title = props.title
+    const image = props.image
     const major = props.major
-    const collegeandyear = props.collegeandyear
+    const college = props.college
+    const year = props.year
 
     return(
         <Flex bg="primaryGray" w="20rem" h="25rem" p="1rem"
@@ -14,10 +15,10 @@ function MemberFrame(props){
             <Image src={image} w="19rem" height="21rem" position="relative" />
             <VStack spacing="0.1rem" marginLeft="0.5rem" marginRight="0.5rem">
                 <Text fontFamily="bannerFont" fontSize="md"> {title}: {name} </Text>
-                <Text fontFamily="bannerFont" fontSize="md"> {major}, {collegeandyear} </Text>
+                <Text fontFamily="bannerFont" fontSize="md"> {college} {major} </Text>
+                <Text fontFamily="bannerFont" fontSize="md"> Class of {year} </Text>
             </VStack>
             </VStack>
-
         </Flex>
     )
 }

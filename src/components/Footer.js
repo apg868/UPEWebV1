ab3865cd799b996d0812ae85f868d399fd196103
Sai Nayunipati <sai-nayunipati@gmail.com>
2024-01-20 21:44:17 -0500

    Add CS major guide

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,79 +1,174 @@
 import React from "react";
-import { VStack, HStack, Image, Text, Link as ChakraLink, Icon, Stack } from "@chakra-ui/react";
+import {
+  VStack,
+  HStack,
+  Image,
+  Text,
+  Link as ChakraLink,
+  Icon,
+  Stack,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logoWHITE from "./../assets_folder/logoWHITE.png";
-import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+  FaEnvelope,
+} from "react-icons/fa";
 
 function Footer() {
-    return (
-        <Stack
-            direction={["column", "column", "row"]} // Adjust the layout based on screen size
-            spacing={[4, 4, "8rem"]}
-            marginTop="3rem"
-            height="auto" // Adjust height for mobile responsiveness
-            width="100%"
-            bgColor="rgba(189, 195, 199, 0.3)"
-            borderColor="rgba(189, 195, 199)"
-            justifyContent="center"
-            p={[4, 8]} // Padding adjustments for different screen sizes
-            borderRadius="10px"
-            alignItems={["center", "center", "flex-start"]} // Center align items only in mobile state
+  return (
+    <Stack
+      direction={["column", "column", "row"]} // Adjust the layout based on screen size
+      spacing={[4, 4, "8rem"]}
+      marginTop="3rem"
+      height="auto" // Adjust height for mobile responsiveness
+      width="100%"
+      bgColor="rgba(189, 195, 199, 0.3)"
+      borderColor="rgba(189, 195, 199)"
+      justifyContent="center"
+      p={[4, 8]} // Padding adjustments for different screen sizes
+      borderRadius="10px"
+      alignItems={["center", "center", "flex-start"]} // Center align items only in mobile state
+    >
+      <Image
+        src={logoWHITE}
+        width={["100px", "150px", "200px"]}
+        height={["80px", "120px", "160px"]}
+        opacity="60%"
+        borderRadius={10}
+      />
+      <VStack spacing={2} align={["center", "center", "flex-start"]}>
+        <Text
+          color="black"
+          fontFamily="secondaryFont"
+          fontWeight="bold"
+          fontSize="md"
         >
-            <Image src={logoWHITE} width={["100px", "150px", "200px"]} height={["80px", "120px", "160px"]} opacity="60%" borderRadius={10} />
-            <VStack spacing={2} align={["center", "center", "flex-start"]}>
-                <Text color="black" fontFamily="secondaryFont" fontWeight="bold" fontSize="md">
-                    Navigation
-                </Text>
-                <Text fontFamily="primaryFont" fontWeight="medium" fontSize="md" color="gray" _hover={{textDecoration: "underline", textDecorationColor: "grey"}}>
-                    <Link to="/">Home</Link>
-                </Text>
-                <Text fontFamily="primaryFont" fontWeight="medium" fontSize="md" color="gray" _hover={{textDecoration: "underline", textDecorationColor: "grey"}}>
-                    <Link to="/members">Members</Link>
-                </Text>
-                <Text fontFamily="primaryFont" fontWeight="medium" fontSize="md" color="gray" _hover={{textDecoration: "underline", textDecorationColor: "grey"}}>
-                    <Link to="/intern-projects">Intern Projects</Link>
-                </Text>
-                <Text fontFamily="primaryFont" fontWeight="medium" fontSize="md" color="gray" _hover={{textDecoration: "underline", textDecorationColor: "grey"}}>
-                    <Link to="/about-us">About Us</Link>
-                </Text>
-            </VStack>
-            <VStack spacing={2} align={["center", "center", "flex-start"]}>
-                <Text color="black" fontFamily="secondaryFont" fontWeight="bold" fontSize="md">
-                    Connect with us on social media!
-                </Text>
-                <HStack spacing={1}>
-                    <ChakraLink href="https://instagram.com/upebu" color="gray" fontFamily="primaryFont" fontWeight="bold" fontSize="md">
-                        Instagram
-                    </ChakraLink>
-                    <Icon as={FaInstagram} boxSize="15px" />
-                </HStack>
-                <HStack spacing={1}>
-                    <ChakraLink href="https://www.facebook.com/upeatbu/" color="gray" fontFamily="primaryFont" fontWeight="bold" fontSize="md">
-                        Facebook
-                    </ChakraLink>
-                    <Icon as={FaFacebook} boxSize="15px" />
-                </HStack>
-                <HStack spacing={1}>
-                    <ChakraLink href="https://x.com/bu_upe" color="gray" fontFamily="primaryFont" fontWeight="bold" fontSize="md">
-                        Twitter
-                    </ChakraLink>
-                    <Icon as={FaTwitter} boxSize="15px" />
-                </HStack>
-                <HStack spacing={1}>
-                    <ChakraLink href="https://www.linkedin.com/company/buupe/" color="gray" fontFamily="primaryFont" fontWeight="bold" fontSize="md">
-                        LinkedIn
-                    </ChakraLink>
-                    <Icon as={FaLinkedin} boxSize="15px" />
-                </HStack>
-                <HStack spacing={1}>
-                    <ChakraLink href="mailto:upatbu@gmail.com" color="gray" fontFamily="primaryFont" fontWeight="bold" fontSize="md">
-                        Email
-                    </ChakraLink>
-                    <Icon as={FaEnvelope} boxSize="15px" />
-                </HStack>
-            </VStack>
-        </Stack>
-    );
+          Navigation
+        </Text>
+        <Text
+          fontFamily="primaryFont"
+          fontWeight="medium"
+          fontSize="md"
+          color="gray"
+          _hover={{ textDecoration: "underline", textDecorationColor: "grey" }}
+        >
+          <Link to="/">Home</Link>
+        </Text>
+        <Text
+          fontFamily="primaryFont"
+          fontWeight="medium"
+          fontSize="md"
+          color="gray"
+          _hover={{ textDecoration: "underline", textDecorationColor: "grey" }}
+        >
+          <Link to="/members">Members</Link>
+        </Text>
+        <Text
+          fontFamily="primaryFont"
+          fontWeight="medium"
+          fontSize="md"
+          color="gray"
+          _hover={{ textDecoration: "underline", textDecorationColor: "grey" }}
+        >
+          <Link to="/intern-projects">Intern Projects</Link>
+        </Text>
+        <Text
+          fontFamily="primaryFont"
+          fontWeight="medium"
+          fontSize="md"
+          color="gray"
+          _hover={{ textDecoration: "underline", textDecorationColor: "grey" }}
+        >
+          <Link to="/about-us">About Us</Link>
+        </Text>
+        <Text
+          fontFamily="primaryFont"
+          fontWeight="medium"
+          fontSize="md"
+          color="gray"
+          _hover={{ textDecoration: "underline", textDecorationColor: "grey" }}
+        >
+          <Link to="https://github.com/sai-nayunipati/upe-bu-cs-major-guide">
+            UPE@BU C.S. Major Guide
+          </Link>
+        </Text>
+      </VStack>
+      <VStack spacing={2} align={["center", "center", "flex-start"]}>
+        <Text
+          color="black"
+          fontFamily="secondaryFont"
+          fontWeight="bold"
+          fontSize="md"
+        >
+          Connect with us on social media!
+        </Text>
+        <HStack spacing={1}>
+          <ChakraLink
+            href="https://instagram.com/upebu"
+            color="gray"
+            fontFamily="primaryFont"
+            fontWeight="bold"
+            fontSize="md"
+          >
+            Instagram
+          </ChakraLink>
+          <Icon as={FaInstagram} boxSize="15px" />
+        </HStack>
+        <HStack spacing={1}>
+          <ChakraLink
+            href="https://www.facebook.com/upeatbu/"
+            color="gray"
+            fontFamily="primaryFont"
+            fontWeight="bold"
+            fontSize="md"
+          >
+            Facebook
+          </ChakraLink>
+          <Icon as={FaFacebook} boxSize="15px" />
+        </HStack>
+        <HStack spacing={1}>
+          <ChakraLink
+            href="https://x.com/bu_upe"
+            color="gray"
+            fontFamily="primaryFont"
+            fontWeight="bold"
+            fontSize="md"
+          >
+            Twitter
+          </ChakraLink>
+          <Icon as={FaTwitter} boxSize="15px" />
+        </HStack>
+        <HStack spacing={1}>
+          <ChakraLink
+            href="https://www.linkedin.com/company/buupe/"
+            color="gray"
+            fontFamily="primaryFont"
+            fontWeight="bold"
+            fontSize="md"
+          >
+            LinkedIn
+          </ChakraLink>
+          <Icon as={FaLinkedin} boxSize="15px" />
+        </HStack>
+        <HStack spacing={1}>
+          <ChakraLink
+            href="mailto:upatbu@gmail.com"
+            color="gray"
+            fontFamily="primaryFont"
+            fontWeight="bold"
+            fontSize="md"
+          >
+            Email
+          </ChakraLink>
+          <Icon as={FaEnvelope} boxSize="15px" />
+        </HStack>
+      </VStack>
+    </Stack>
+  );
 }
 
 export default Footer;
--- src/components/MainPage/CTA/CTAOrg/CTAMobile.js
+++ src/components/MainPage/CTA/CTAOrg/CTAMobile.js
@@ -1,73 +1,97 @@
-import { VStack, Flex, Text, Button} from "@chakra-ui/react";
+import { VStack, Flex, Text, Button } from "@chakra-ui/react";
 
-
-function CTAMobile(){
-    return(
-        <Flex
-            position="relative"
-            top="0"
-            right="0"
-            bottom="0"
-            left="0"
-            justifyContent="center"
-            alignItems="center"
-            maxWidth="90%"
-            marginX="auto"
-        >
-            <Flex
-                bg="primaryRed"
-                width="95%"
-                height="30em"
-                marginTop="5em"
-                justifyContent="center"
-                borderRadius="2em"
-                p="1.5em"
-                align="center"
-                alignSelf="center"
+function CTAMobile() {
+  return (
+    <Flex
+      position="relative"
+      top="0"
+      right="0"
+      bottom="0"
+      left="0"
+      justifyContent="center"
+      alignItems="center"
+      maxWidth="90%"
+      marginX="auto"
+    >
+      <Flex
+        bg="primaryRed"
+        width="95%"
+        height="30em"
+        marginTop="5em"
+        justifyContent="center"
+        borderRadius="2em"
+        p="1.5em"
+        align="center"
+        alignSelf="center"
+      >
+        <VStack spacing="3em">
+          <Text
+            color="white"
+            fontSize="3xl"
+            fontFamily="secondaryFont"
+            fontWeight="medium"
+          >
+            Want to Join?
+          </Text>
+          <VStack spacing="2.5em">
+            <a
+              href="https://docs.google.com/forms/d/1HjThY70Ucu2ne6P3kPqFxn31WBTdu9P12SGX5YFTTE8/closedform"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-                <VStack spacing="3em">
-                    <Text color="white" fontSize="3xl" fontFamily="secondaryFont" fontWeight="medium">
-                        Want to Join?
-                    </Text>
-                    <VStack spacing="2.5em">
-                        <a href="https://docs.google.com/forms/d/1HjThY70Ucu2ne6P3kPqFxn31WBTdu9P12SGX5YFTTE8/closedform" target="_blank" rel="noopener noreferrer">
-                            <Button
-                                bg="white"
-                                _hover={{ bg: "secondaryRed", textColor: "white"}}
-                                borderRadius="2em"
-                                fontFamily="secondaryFont"
-                                fontWeight="bold"
-                                color="primaryRed"
-                                h="3em"
-                                w="15em"
-                                fontSize="md"
-                            >
-                                Application
-                            </Button>
-                        </a>
-                        <a href="https://www.signupgenius.com/go/10C084EA5AA29A2F4CF8-upeinterview#" target="_blank" rel="noopener noreferrer">
-                            <Button
-                                bg="white"
-                                _hover={{ bg: "secondaryRed", textColor: "white"}}
-                                borderRadius="2em"
-                                fontFamily="secondaryFont"
-                                fontWeight="bold"
-                                color="primaryRed"
-                                h="3em"
-                                w="15em"
-                                fontSize="md"
-                            >
-                                Interview Sign-up
-                            </Button>
-                        </a>
-                    </VStack>
-                    <Text color="white" fontSize="xl" fontFamily="secondaryFont" fontWeight="medium" maxWidth="50%" textAlign="center">
-                        Our applications open every fall and spring semester
-                    </Text>
-                </VStack>
-            </Flex>
-        </Flex>
-    )
+              <Button
+                bg="white"
+                _hover={{ bg: "secondaryRed", textColor: "white" }}
+                borderRadius="2em"
+                fontFamily="secondaryFont"
+                fontWeight="bold"
+                color="primaryRed"
+                h="3em"
+                w="15em"
+                fontSize="md"
+              >
+                Application
+              </Button>
+            </a>
+            <a
+              href="https://www.signupgenius.com/go/10C084EA5AA29A2F4CF8-upeinterview#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                bg="white"
+                _hover={{ bg: "secondaryRed", textColor: "white" }}
+                borderRadius="2em"
+                fontFamily="secondaryFont"
+                fontWeight="bold"
+                color="primaryRed"
+                h="3em"
+                w="15em"
+                fontSize="md"
+              >
+                Interview Sign-up
+              </Button>
+            </a>
+          </VStack>
+          <a
+            href="https://www.signupgenius.com/go/10C084EA5AA29A2F4CF8-upeinterview#"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          <Text
+            color="white"
+            fontSize="xl"
+            fontFamily="secondaryFont"
+            fontWeight="medium"
+            maxWidth="50%"
+            textAlign="center"
+          >
+            Our applications open every fall and spring semester
+          </Text>
+        </VStack>
+      </Flex>
+    </Flex>
+  );
 }
 
-export default CTAMobile
+export default CTAMobile;

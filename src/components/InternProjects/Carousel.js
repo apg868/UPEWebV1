e322ce594b3dc3d6f5ced55605fb714b0b8419fc
apg868 <123202954+apg868@users.noreply.github.com>
2024-01-21 18:45:47 -0500

    scalability updates to interactive carousel

--- src/components/InternProjects/Carousel.js
+++ src/components/InternProjects/Carousel.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, VStack, Text, Flex, CloseButton, HStack, Icon } from "@chakra-ui/react";
+import {
+    Box, Button, VStack, Text, Flex, CloseButton, HStack, Icon,
+    Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, useMediaQuery
+} from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon, SettingsIcon, TimeIcon, SpinnerIcon, InfoIcon } from "@chakra-ui/icons";
 
 function Slide({ slide, handlePrev, handleNext, handleFlip, isFlipped }) {
+    const [modalInfo, setModalInfo] = useState({ isOpen: false, content: '' });
+    const [isLargerThanBase] = useMediaQuery("(min-width: 48em)");
+
+    const handleModal = (content = '') => {
+        setModalInfo({
+            isOpen: !modalInfo.isOpen,
+            content: content
+        });
+    };
+
     return (
         <Box position="relative" w={isFlipped ? "75vw" : "75vw"} h={isFlipped ? "75vh" : "75vh"} bgImage={`url(${slide.image || ""})`} bgSize="cover" bgPosition="center">
             {!isFlipped && (
                 <>
-                    {/* Slide Content */}
                     <VStack
                         spacing={3}
                         position="absolute"
@@ -15,54 +27,78 @@ function Slide({ slide, handlePrev, handleNext, handleFlip, isFlipped }) {
                         left="5%"
                         bg="rgba(0,0,0,0.7)"
                         p={{ base: "0.5", md: "1", lg: "2" }}
-                        w={{ base: "10em", md: "15em", lg: "20em" }}
+                        w={{ base: "10rem", md: "10rem", lg: "20em" }}
                         borderRadius="md"
                         alignItems="left"
                     >
-                        <Text fontSize={{ base: "lg", md: "xl", lg: "2xl" }} fontFamily="primaryFont" fontWeight="medium" color="white">{slide.name}</Text>
-                        <Text fontSize={{ base: "md", md: "lg", lg: "xl" }} fontFamily="bannerFont" color="white">{slide.description}</Text>
+                        <Text fontSize={{ base: "sm", md: "md", lg: "lg" }} fontFamily="primaryFont" fontWeight="medium" color="white">{slide.name}</Text>
+                        <Text fontSize={{ base: "xs", md: "sm", lg: "md" }} fontFamily="bannerFont" color="white">{slide.description}</Text>
                     </VStack>
                     <NavigationButton direction="left" handleClick={handlePrev} />
                     <NavigationButton direction="right" handleClick={handleNext} />
-                    <Button position="absolute" bottom="5%" right="5%" colorScheme="gray" onClick={handleFlip} _hover={{ backgroundColor: "secondaryRed", color: "white" }}>Read More</Button>
+                    <Button
+                        fontSize={{ "base": "md" }}
+                        size={{ "base": "xs", "md": "md", "lg": "lg" }}
+                        position="absolute"
+                        top={{ "base": "0.5rem", "md": "auto" }}
+                        left={{ "base": "0.5rem", "md": "auto" }}
+                        bottom={{ "base": "auto", "md": "0.5rem" }}
+                        right={{ "base": "auto", "md": "0.5rem" }}
+                        colorScheme="gray"
+                        onClick={handleFlip}
+                        _hover={{ backgroundColor: "secondaryRed", color: "white" }}
+                    >
+                        Read More
+                    </Button>
                 </>
             )}
 
-{isFlipped && (
-    <>
-    <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" w="70%" h="70%" bg="whiteAlpha.900" p={4} borderRadius="md" border="3px solid" borderColor="primaryRed" dropShadow="3px">
-        <VStack spacing={{"base":"3em","md":"4em","lg":"4em"}} align="left"
-        fontSize={{"base":"xs","md":"sm","lg":"md"}} fontFamily="primaryFont">
-            <HStack spacing={{"base":"2em", "md":"4em","lg":"1.5em"}}>
-                <Icon as={InfoIcon} />
-                <Text fontWeight="bold" > Extended Description: </Text>
-                <Text>{slide.extendedDescription}</Text>
-            </HStack>
-            <HStack spacing={{"base":"2em", "md":"4em","lg":"1.5em"}}>
-                <Icon as={SpinnerIcon} />
-                <Text fontWeight="bold"> Challenges Faced: </Text>
-                <Text >{slide.challenges}</Text>
-            </HStack>
-            <HStack spacing={{"base":"2em", "md":"4em","lg":"1.5em"}}>
-                <Icon as={SettingsIcon} />
-                <Text fontWeight="bold"> Main Technologies: </Text>
-                <Text fontSize="lg">{slide.technologies}</Text>
-            </HStack>
-            <HStack spacing={{"base":"2em", "md":"4em","lg":"1.5em"}}>
-                <Icon as={TimeIcon} />
-                <Text fontWeight="bold"> Time Invested: </Text>
-                <Text fontSize="lg">{slide.time}</Text>
-            </HStack>
-        </VStack>
-    </Box>
-    {/* Customized Close Button */}
-    <Box position="absolute" top="2%" right="2%" bg="white" borderRadius="50%" >
-        <CloseButton size="lg" color="black" onClick={handleFlip} />
-    </Box>
-</>
-
-)}
-
+            {isFlipped && (
+                <>
+                    <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" w="70%" h="70%" bg="whiteAlpha.900" p={4} borderRadius="md" border="3px solid" borderColor="primaryRed" dropShadow="3px"
+                    >
+                        <VStack spacing={{ "base": "1.5em", "sm":"2em", "md": "2em", "lg": "4em" }} align="left" fontSize={{ "base": "2xs","sm":"sm", "md": "xs", "lg": "md" }} fontFamily="primaryFont"
+                        alignItems={{ base: "center", md: "flex-start" }}
+                        width="100%"
+                        height="100%"
+                        justifyContent={{"base":"center","md":"flex-start"}}>
+                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.extendedDescription)}>
+                                <Icon as={InfoIcon} />
+                                <Text fontWeight="bold">Extended Description</Text>
+                                {isLargerThanBase && <Text>{slide.extendedDescription}</Text>}
+                            </HStack>
+                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.challenges)}>
+                                <Icon as={SpinnerIcon} />
+                                <Text fontWeight="bold">Challenges Faced</Text>
+                                {isLargerThanBase && <Text>{slide.challenges}</Text>}
+                            </HStack>
+                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.technologies)}>
+                                <Icon as={SettingsIcon} />
+                                <Text fontWeight="bold">Main Technologies</Text>
+                                {isLargerThanBase && <Text>{slide.technologies}</Text>}
+                            </HStack>
+                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.time)}>
+                                <Icon as={TimeIcon} />
+                                <Text fontWeight="bold">Time Invested</Text>
+                                {isLargerThanBase && <Text>{slide.time}</Text>}
+                            </HStack>
+                        </VStack>
+                    </Box>
+                    <Modal isOpen={modalInfo.isOpen} onClose={() => handleModal()}>
+                        <ModalOverlay/>
+                        <ModalContent fontFamily="secondaryFont" >
+                            <ModalHeader></ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                {modalInfo.content}
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
+                    <Box position="absolute" top="2%" right="2%" bg="white" borderRadius="50%" >
+                        <CloseButton size={{"base":"md","md":"md","lg":"lg"}} color="black" onClick={handleFlip} />
+                    </Box>
+                </>
+            )}
         </Box>
     );
 }
@@ -71,23 +107,16 @@ function NavigationButton({ direction, handleClick }) {
     const isLeft = direction === "left";
     const Icon = isLeft ? ChevronLeftIcon : ChevronRightIcon;
 
-    // Create a style object
     let buttonStyle = {
         position: "absolute",
         top: "50%",
         transform: "translateY(-50%)",
         variant: "ghost",
+        [isLeft ? "left" : "right"]: "2%"
     };
 
-    // Dynamically set the left or right property
-    buttonStyle[isLeft ? "left" : "right"] = "2%";
-
     return (
-        <Button
-            {...buttonStyle}
-            onClick={handleClick}
-            _hover={{ backgroundColor: "transparent" }}
-        >
+        <Button {...buttonStyle} onClick={handleClick} _hover={{ backgroundColor: "transparent" }}>
             <Box bg="rgba(255,255,255,0.9)" borderRadius="50%" p={1}>
                 <Icon w={{ base: "5", md: "6", lg: "9" }} h={{ base: "5", md: "6", lg: "9" }} color="darkred" />
             </Box>
--- src/components/InternProjects/InternButton.js
+++ src/components/InternProjects/InternButton.js
@@ -2,18 +2,22 @@ import { Text, Flex } from "@chakra-ui/react";
 
 function InternBox() {
     return (
-        <Flex justifyContent="center" w="60%">
+        <Flex justifyContent="center">
             <Flex
                 color="primaryRed"
                 fontFamily="bannerFont"
-                w={{"base":"20em","md":"30em", "lg":"50em"}}
-                h={{"base":"10em","md":"30em", "lg":"3em"}}
+                fontWeight="semibold"
+                textAlign="center"
+                w={{"base":"16em","md":"40em", "lg":"50em"}}
+                h={{"base":"5em","md":"5em", "lg":"5em"}}
                 mb={{"base":"2em","md":"2em","lg":"2em"}}
                 mt={{"base":"2em","md":"2em","lg":"2em"}}
                 alignItems="center"
                 justifyContent="center"
             >
-                <Text fontSize={{"base":"xl","md":"2xl","lg":"3xl"}} fontFamily="bannerFont">See Some of Our Highlighted Projects Below</Text>
+                <Text fontSize={{"base":"2xl","md":"3xl","lg":"3xl"}} fontFamily="bannerFont"
+                textAlign="center"
+                wordWrap="break-word">See Some of Our Highlighted Projects Below</Text>
             </Flex>
         </Flex>
     );

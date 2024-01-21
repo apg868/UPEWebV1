964671e087f3e9fb329ab5ea696561caf5bacb77
Sai Nayunipati <sai-nayunipati@gmail.com>
2024-01-20 20:37:19 -0500

    Populate members page

--- src/components/MembersPage/MemberFrame.js
+++ src/components/MembersPage/MemberFrame.js
@@ -1,25 +1,53 @@
 import { Flex, Text, Image, VStack } from "@chakra-ui/react";
 
-function MemberFrame({ student, image }) {
-    const name = student.name;
-    const title = student.title
-    const major = student.major
-    const college = student.college
-    const year = student.year
+import placeholderAvatar from "./../../assets_folder/user.svg";
 
-    return (
-        <Flex bg="primaryGray" w="20rem" h="25rem" p="1rem"
-            justifyContent="center" >
-            <VStack spacing="0.2rem">
-                <Image src={image} w="19rem" height="21rem" position="relative" />
-                <VStack spacing="0.1rem" marginLeft="0.5rem" marginRight="0.5rem">
-                    <Text fontFamily="bannerFont" fontSize="md"> {title}: {name} </Text>
-                    <Text fontFamily="bannerFont" fontSize="md"> {college} {major} </Text>
-                    <Text fontFamily="bannerFont" fontSize="md"> Class of {year} </Text>
-                </VStack>
-            </VStack>
-        </Flex>
-    )
+function MemberFrame({ student, image }) {
+  const name = student.name;
+  const title = student.title ?? null;
+  const major = student.major ?? null;
+  const year = student.year ?? null;
+
+  var avatar;
+
+  try {
+    avatar = require(`./../../assets_folder/student_images/${image}`);
+  } catch {
+    avatar = placeholderAvatar;
+  }
+
+  return (
+    <Flex bg="primaryGray" w="24rem" h="27rem" p="1rem" justifyContent="center">
+      <VStack spacing="0.2rem">
+        <Image
+          fit={"cover"}
+          src={avatar}
+          w="19rem"
+          height="19rem"
+          position="relative"
+        />
+        <VStack spacing="0.1rem" marginLeft="0.5rem" marginRight="0.5rem">
+          <Text fontFamily="bannerFont" fontSize="md">
+            {" "}
+            {name}{" "}
+          </Text>
+          <Text fontFamily="bannerFont" fontSize="md">
+            {" "}
+            {title}
+          </Text>
+          <Text fontFamily="bannerFont" fontSize="sm">
+            {" "}
+            {major}{" "}
+          </Text>
+          <Text fontFamily="bannerFont" fontSize="md">
+            {" "}
+            {year && "Class of "}
+            {year}{" "}
+          </Text>
+        </VStack>
+      </VStack>
+    </Flex>
+  );
 }
 
-export default MemberFrame
+export default MemberFrame;
--- src/components/MembersPage/MembersPage.js
+++ src/components/MembersPage/MembersPage.js
@@ -1,80 +1,97 @@
 import React, { useState } from "react";
 
-import { HStack, VStack, Text, Button, Collapse, Box, useBreakpointValue } from "@chakra-ui/react";
+import {
+  HStack,
+  VStack,
+  Text,
+  Button,
+  Collapse,
+  Box,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 import IntroBanner from "./IntroBanner";
 import MemberFrame from "./MemberFrame";
 
 import students from "../../assets_folder/students.json";
-import profile1 from "./../../assets_folder/tempProfile.jpg";
 
 function MembersPage() {
-    const [funFactVisibilities, setFunFactVisibilities] = useState(students.map(() => false));
+  const [funFactVisibilities, setFunFactVisibilities] = useState(
+    students.map(() => false)
+  );
 
-    const toggleFunFact = (index) => {
-        const newVisibilities = [...funFactVisibilities];
-        newVisibilities[index] = !newVisibilities[index];
-        setFunFactVisibilities(newVisibilities);
-    };
+  const toggleFunFact = (index) => {
+    const newVisibilities = [...funFactVisibilities];
+    newVisibilities[index] = !newVisibilities[index];
+    setFunFactVisibilities(newVisibilities);
+  };
 
-    const breakpoint = useBreakpointValue({ base: "base", md: "md", lg: "lg" });
+  const breakpoint = useBreakpointValue({ base: "base", md: "md", lg: "lg" });
 
-    let ROW_LENGTH;
-    switch (breakpoint) {
-        case "lg":
-            ROW_LENGTH = 3;
-            break;
-        case "md":
-            ROW_LENGTH = 2;
-            break;
-        case "base":
-            ROW_LENGTH = 1;
-            break;
-    }
+  let ROW_LENGTH;
+  switch (breakpoint) {
+    case "lg":
+      ROW_LENGTH = 3;
+      break;
+    case "md":
+      ROW_LENGTH = 2;
+      break;
+    case "base":
+      ROW_LENGTH = 1;
+      break;
+  }
 
+  let officers = students.filter((x) => x.title !== "Member");
+  let officerRows = [];
+  for (let i = 0; i < officers.length; i += ROW_LENGTH) {
+    officerRows.push(officers.slice(i, i + ROW_LENGTH));
+  }
 
-    let officers = students.filter(x => x.title !== "member");
-    let officerRows = [];
-    for (let i = 0; i < officers.length; i += ROW_LENGTH) {
-        officerRows.push(officers.slice(i, i + ROW_LENGTH));
-    }
+  let plebs = students.filter((x) => x.title == "Member");
+  let plebRows = [];
+  for (let i = 0; i < plebs.length; i += ROW_LENGTH) {
+    plebRows.push(plebs.slice(i, i + ROW_LENGTH));
+  }
 
-    let plebs = students.filter(x => x.title == "member");
-    let plebRows = [];
-    for (let i = 0; i < plebs.length; i += ROW_LENGTH) {
-        plebRows.push(plebs.slice(i, i + ROW_LENGTH));
-    }
-
-    function getStudentGrid(studentRows) {
-        return studentRows.map((row, i) => {
+  function getStudentGrid(studentRows) {
+    return studentRows.map((row, i) => {
+      return (
+        <HStack spacing="4rem">
+          {row.map((student, j) => {
+            let student_index = i * ROW_LENGTH + j;
             return (
-                <HStack spacing="4rem">
-                    {row.map((student, j) => {
-                        let student_index = i * ROW_LENGTH + j;
-                        return (
-                            <VStack>
-                                <MemberFrame student={student} image={profile1} />
-                                <Button onClick={() => toggleFunFact(student_index)} colorScheme="teal">About Me</Button>
-                                <Collapse in={funFactVisibilities[student_index]}>
-                                    <Box p={4} mt={4} borderWidth="1px" borderRadius="md">
-                                        <Text fontFamily="bannerFont" fontSize="md"> {student.funfact} </Text>
-                                    </Box>
-                                </Collapse>
-                            </VStack>
-                        )
-                    })}
-                </HStack>
-            )
-        })
-    }
+              <VStack>
+                <MemberFrame student={student} image={student.image_path} />
+                <Button
+                  isDisabled={!student.funfact || student.funfact.length === 0}
+                  onClick={() => toggleFunFact(student_index)}
+                  colorScheme="teal"
+                >
+                  About Me
+                </Button>
+                <Collapse in={funFactVisibilities[student_index]}>
+                  <Box p={4} mt={4} borderWidth="1px" borderRadius="md">
+                    <Text fontFamily="bannerFont" fontSize="md">
+                      {" "}
+                      {student.funfact}{" "}
+                    </Text>
+                  </Box>
+                </Collapse>
+              </VStack>
+            );
+          })}
+        </HStack>
+      );
+    });
+  }
 
-    return (
-        <VStack spacing="2rem" paddingTop="3rem">
-            <IntroBanner content={"E-Board"} />
-            {getStudentGrid(officerRows)}
-            <IntroBanner content={"Current Members"} />
-            {getStudentGrid(plebRows)}
-        </VStack>
-    )
+  return (
+    <VStack spacing="2rem" paddingTop="3rem">
+      <IntroBanner content={"E-Board"} />
+      {getStudentGrid(officerRows)}
+      <IntroBanner content={"Current Members"} />
+      {getStudentGrid(plebRows)}
+    </VStack>
+  );
 }
-export default MembersPage
+export default MembersPage;

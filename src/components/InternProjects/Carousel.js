9fae69920feed40d3f4615dd18bba846a9fdcbf7
Sai Nayunipati <sai-nayunipati@gmail.com>
2024-01-23 19:51:08 -0500

    Minor polish

--- src/components/InternProjects/Carousel.js
+++ src/components/InternProjects/Carousel.js
@@ -1,179 +1,283 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    Box, Button, VStack, Text, Flex, CloseButton, HStack, Icon,
-    Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, useMediaQuery
+  Box,
+  Button,
+  VStack,
+  Text,
+  Flex,
+  CloseButton,
+  HStack,
+  Icon,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  useMediaQuery,
 } from "@chakra-ui/react";
-import { ChevronLeftIcon, ChevronRightIcon, SettingsIcon, TimeIcon, SpinnerIcon, InfoIcon } from "@chakra-ui/icons";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  SettingsIcon,
+  TimeIcon,
+  SpinnerIcon,
+  InfoIcon,
+} from "@chakra-ui/icons";
 
 function Slide({ slide, handlePrev, handleNext, handleFlip, isFlipped }) {
-    const [modalInfo, setModalInfo] = useState({ isOpen: false, content: '' });
-    const [isLargerThanBase] = useMediaQuery("(min-width: 48em)");
+  const [modalInfo, setModalInfo] = useState({ isOpen: false, content: "" });
+  const [isLargerThanBase] = useMediaQuery("(min-width: 48em)");
 
-    const handleModal = (content = '') => {
-        setModalInfo({
-            isOpen: !modalInfo.isOpen,
-            content: content
-        });
-    };
+  const handleModal = (content = "") => {
+    setModalInfo({
+      isOpen: !modalInfo.isOpen,
+      content: content,
+    });
+  };
 
-    return (
-        <Box position="relative" w={isFlipped ? "75vw" : "75vw"} h={isFlipped ? "75vh" : "75vh"} bgImage={`url(${slide.image || ""})`} bgSize="cover" bgPosition="center">
-            {!isFlipped && (
-                <>
-                    <VStack
-                        spacing={3}
-                        position="absolute"
-                        bottom="10%"
-                        left="5%"
-                        bg="rgba(0,0,0,0.8)"
-                        p={{ base: "0.5", md: "1", lg: "2" }}
-                        w={{ base: "10rem", md: "10rem", lg: "20em" }}
-                        borderRadius="md"
-                        alignItems="left"
-                    >
-                        <Text fontSize={{ base: "sm", md: "md", lg: "lg" }} fontFamily="primaryFont" fontWeight="medium" color="white">{slide.name}</Text>
-                        <Text fontSize={{ base: "xs", md: "sm", lg: "md" }} fontFamily="bannerFont" color="white">{slide.description}</Text>
-                    </VStack>
-                    <NavigationButton direction="left" handleClick={handlePrev} />
-                    <NavigationButton direction="right" handleClick={handleNext} />
-                    <Button
-                        fontSize={{ "base": "md" }}
-                        size={{ "base": "xs", "md": "md", "lg": "lg" }}
-                        position="absolute"
-                        top={{ "base": "0.5rem", "md": "auto" }}
-                        left={{ "base": "0.5rem", "md": "auto" }}
-                        bottom={{ "base": "auto", "md": "0.5rem" }}
-                        right={{ "base": "auto", "md": "0.5rem" }}
-                        colorScheme="gray"
-                        onClick={handleFlip}
-                        _hover={{ backgroundColor: "secondaryRed", color: "white" }}
-                    >
-                        Read More
-                    </Button>
-                </>
-            )}
+  return (
+    <Box
+      position="relative"
+      w={isFlipped ? "75vw" : "75vw"}
+      h={isFlipped ? "75vh" : "75vh"}
+      bgImage={`url(${slide.image || ""})`}
+      bgSize="cover"
+      bgPosition="center"
+    >
+      {!isFlipped && (
+        <>
+          <VStack
+            spacing={3}
+            position="absolute"
+            bottom="10%"
+            left="5%"
+            bg="rgba(0,0,0,0.8)"
+            p={{ base: "0.5", md: "1", lg: "2" }}
+            w={{ base: "10rem", md: "10rem", lg: "20em" }}
+            borderRadius="md"
+            alignItems="left"
+          >
+            <Text
+              fontSize={{ base: "sm", md: "md", lg: "lg" }}
+              fontFamily="primaryFont"
+              fontWeight="medium"
+              color="white"
+            >
+              {slide.name}
+            </Text>
+            <Text
+              fontSize={{ base: "xs", md: "sm", lg: "md" }}
+              fontFamily="bannerFont"
+              color="white"
+            >
+              {slide.description}
+            </Text>
+          </VStack>
+          <NavigationButton direction="left" handleClick={handlePrev} />
+          <NavigationButton direction="right" handleClick={handleNext} />
+          <Button
+            fontSize={{ base: "md" }}
+            size={{ base: "xs", md: "md", lg: "lg" }}
+            position="absolute"
+            top={{ base: "0.5rem", md: "auto" }}
+            left={{ base: "0.5rem", md: "auto" }}
+            bottom={{ base: "auto", md: "0.5rem" }}
+            right={{ base: "auto", md: "0.5rem" }}
+            colorScheme="gray"
+            onClick={handleFlip}
+            _hover={{ backgroundColor: "secondaryRed", color: "white" }}
+          >
+            Read More
+          </Button>
+        </>
+      )}
 
-            {isFlipped && (
-                <>
-                    <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" w="70%" h="70%" bg="whiteAlpha.900" p={4} borderRadius="md" border="3px solid" borderColor="primaryRed" dropShadow="3px"
-                    >
-                        <VStack spacing={{ "base": "1.5em", "sm":"2em", "md": "2em", "lg": "3em" }} align="left" fontSize={{ "base": "2xs","sm":"sm", "md": "xs", "lg": "md" }} fontFamily="primaryFont"
-                        alignItems={{ base: "center", md: "flex-start" }}
-                        width="100%"
-                        height="100%"
-                        justifyContent={{"base":"center","md":"flex-start"}}>
-                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.extendedDescription)}>
-                                <Icon as={InfoIcon} />
-                                <Text fontWeight="bold">Extended Description</Text>
-                                {isLargerThanBase && <Text>{slide.extendedDescription}</Text>}
-                            </HStack>
-                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.challenges)}>
-                                <Icon as={SpinnerIcon} />
-                                <Text fontWeight="bold">Challenges Faced</Text>
-                                {isLargerThanBase && <Text>{slide.challenges}</Text>}
-                            </HStack>
-                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.technologies)}>
-                                <Icon as={SettingsIcon} />
-                                <Text fontWeight="bold">Main Technologies</Text>
-                                {isLargerThanBase && <Text>{slide.technologies}</Text>}
-                            </HStack>
-                            <HStack spacing={{ "base": "2em", "md": "2em", "lg": "1.5em" }} onClick={() => isLargerThanBase ? null : handleModal(slide.time)}>
-                                <Icon as={TimeIcon} />
-                                <Text fontWeight="bold">Time Invested</Text>
-                                {isLargerThanBase && <Text>{slide.time}</Text>}
-                            </HStack>
-                        </VStack>
-                    </Box>
-                    <Modal isOpen={modalInfo.isOpen} onClose={() => handleModal()}>
-                        <ModalOverlay/>
-                        <ModalContent fontFamily="secondaryFont" >
-                            <ModalHeader></ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                {modalInfo.content}
-                            </ModalBody>
-                        </ModalContent>
-                    </Modal>
-                    <Box position="absolute" top="2%" right="2%" bg="white" borderRadius="50%" >
-                        <CloseButton size={{"base":"md","md":"md","lg":"lg"}} color="black" onClick={handleFlip} />
-                    </Box>
-                </>
-            )}
-        </Box>
-    );
+      {isFlipped && (
+        <>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            w="70%"
+            h="70%"
+            bg="whiteAlpha.900"
+            p={4}
+            borderRadius="md"
+            border="3px solid"
+            borderColor="primaryRed"
+            dropShadow="3px"
+          >
+            <VStack
+              spacing={{ base: "1.5em", sm: "2em", md: "2em", lg: "3em" }}
+              align="left"
+              fontSize={{ base: "2xs", sm: "lg", md: "xs", lg: "sm" }}
+              fontFamily="primaryFont"
+              alignItems={{ base: "center", md: "flex-start" }}
+              width="100%"
+              height="100%"
+              justifyContent={{ base: "center", md: "flex-start" }}
+            >
+              <HStack
+                spacing={{ base: "2em", md: "2em", lg: "1.5em" }}
+                onClick={() =>
+                  isLargerThanBase
+                    ? null
+                    : handleModal(slide.extendedDescription)
+                }
+              >
+                <Icon as={InfoIcon} />
+                <Text fontWeight="bold">Extended Description</Text>
+                {isLargerThanBase && <Text>{slide.extendedDescription}</Text>}
+              </HStack>
+              <HStack
+                spacing={{ base: "2em", md: "2em", lg: "1.5em" }}
+                onClick={() =>
+                  isLargerThanBase ? null : handleModal(slide.challenges)
+                }
+              >
+                <Icon as={SpinnerIcon} />
+                <Text fontWeight="bold">Challenges Faced</Text>
+                {isLargerThanBase && <Text>{slide.challenges}</Text>}
+              </HStack>
+              <HStack
+                spacing={{ base: "2em", md: "2em", lg: "1.5em" }}
+                onClick={() =>
+                  isLargerThanBase ? null : handleModal(slide.technologies)
+                }
+              >
+                <Icon as={SettingsIcon} />
+                <Text fontWeight="bold">Main Technologies</Text>
+                {isLargerThanBase && <Text>{slide.technologies}</Text>}
+              </HStack>
+              <HStack
+                spacing={{ base: "2em", md: "2em", lg: "1.5em" }}
+                onClick={() =>
+                  isLargerThanBase ? null : handleModal(slide.time)
+                }
+              >
+                <Icon as={TimeIcon} />
+                <Text fontWeight="bold">Time Invested</Text>
+                {isLargerThanBase && <Text>{slide.time}</Text>}
+              </HStack>
+            </VStack>
+          </Box>
+          <Modal isOpen={modalInfo.isOpen} onClose={() => handleModal()}>
+            <ModalOverlay />
+            <ModalContent fontFamily="secondaryFont">
+              <ModalHeader></ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>{modalInfo.content}</ModalBody>
+            </ModalContent>
+          </Modal>
+          <Box
+            position="absolute"
+            top="2%"
+            right="2%"
+            bg="white"
+            borderRadius="50%"
+          >
+            <CloseButton
+              size={{ base: "md", md: "md", lg: "lg" }}
+              color="black"
+              onClick={handleFlip}
+            />
+          </Box>
+        </>
+      )}
+    </Box>
+  );
 }
 
 function NavigationButton({ direction, handleClick }) {
-    const isLeft = direction === "left";
-    const Icon = isLeft ? ChevronLeftIcon : ChevronRightIcon;
+  const isLeft = direction === "left";
+  const Icon = isLeft ? ChevronLeftIcon : ChevronRightIcon;
 
-    let buttonStyle = {
-        position: "absolute",
-        top: "50%",
-        transform: "translateY(-50%)",
-        variant: "ghost",
-        [isLeft ? "left" : "right"]: "2%"
-    };
+  let buttonStyle = {
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    variant: "ghost",
+    [isLeft ? "left" : "right"]: "2%",
+  };
 
-    return (
-        <Button {...buttonStyle} onClick={handleClick} _hover={{ backgroundColor: "transparent" }}>
-            <Box bg="rgba(255,255,255,0.9)" borderRadius="50%" p={1}>
-                <Icon w={{ base: "5", md: "6", lg: "9" }} h={{ base: "5", md: "6", lg: "9" }} color="darkred" />
-            </Box>
-        </Button>
-    );
+  return (
+    <Button
+      {...buttonStyle}
+      onClick={handleClick}
+      _hover={{ backgroundColor: "transparent" }}
+    >
+      <Box bg="rgba(255,255,255,0.9)" borderRadius="50%" p={1}>
+        <Icon
+          w={{ base: "5", md: "6", lg: "9" }}
+          h={{ base: "5", md: "6", lg: "9" }}
+          color="darkred"
+        />
+      </Box>
+    </Button>
+  );
 }
 
 function CaptionCarousel({ slides }) {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [isFlipped, setIsFlipped] = useState(false);
-    const [isAutoScrolling, setIsAutoScrolling] = useState(true);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isAutoScrolling, setIsAutoScrolling] = useState(true);
 
-    const handleFlip = () => {
-        setIsFlipped(!isFlipped);
-        setIsAutoScrolling(!isAutoScrolling); // Stop auto-scrolling when a slide is flipped
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+    setIsAutoScrolling(!isAutoScrolling); // Stop auto-scrolling when a slide is flipped
+  };
+
+  const handlePrev = () => {
+    setCurrentSlide((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
+    setIsAutoScrolling(false); // Stop auto-scrolling on user interaction
+  };
+
+  const handleNext = () => {
+    setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    setIsAutoScrolling(false); // Stop auto-scrolling on user interaction
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isAutoScrolling && !isFlipped) {
+      interval = setInterval(() => {
+        setCurrentSlide((prevSlide) =>
+          prevSlide === slides.length - 1 ? 0 : prevSlide + 1
+        );
+      }, 10000); // Auto-scroll every 10 seconds
+    }
+
+    // Resume autoscrolling after a period of inactivity
+    const resumeAutoScroll = setTimeout(() => {
+      setIsAutoScrolling(true);
+    }, 15000); // 15 seconds of inactivity to resume autoscrolling
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(resumeAutoScroll);
     };
+  }, [isAutoScrolling, isFlipped, slides.length]);
 
-    const handlePrev = () => {
-        setCurrentSlide(prev => (prev === 0 ? slides.length - 1 : prev - 1));
-        setIsAutoScrolling(false); // Stop auto-scrolling on user interaction
-    };
-
-    const handleNext = () => {
-        setCurrentSlide(prev => (prev === slides.length - 1 ? 0 : prev + 1));
-        setIsAutoScrolling(false); // Stop auto-scrolling on user interaction
-    };
-
-    useEffect(() => {
-        let interval = null;
-        if (isAutoScrolling && !isFlipped) {
-            interval = setInterval(() => {
-                setCurrentSlide(prevSlide => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
-            }, 10000); // Auto-scroll every 10 seconds
-        }
-
-        // Resume autoscrolling after a period of inactivity
-        const resumeAutoScroll = setTimeout(() => {
-            setIsAutoScrolling(true);
-        }, 15000); // 15 seconds of inactivity to resume autoscrolling
-
-        return () => {
-            clearInterval(interval);
-            clearTimeout(resumeAutoScroll);
-        };
-    }, [isAutoScrolling, isFlipped, slides.length]);
-
-    return (
-        <Flex justifyContent="center" alignItems="center" w="100%" h="100%" transition="transform 0.6s" transform={isFlipped ? 'scale(1.1)' : 'scale(1)'}>
-            <Slide
-                slide={slides[currentSlide]}
-                handlePrev={handlePrev}
-                handleNext={handleNext}
-                handleFlip={handleFlip}
-                isFlipped={isFlipped}
-            />
-        </Flex>
-    );
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      w="100%"
+      h="100%"
+      transition="transform 0.6s"
+      transform={isFlipped ? "scale(1.1)" : "scale(1)"}
+    >
+      <Slide
+        slide={slides[currentSlide]}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+        handleFlip={handleFlip}
+        isFlipped={isFlipped}
+      />
+    </Flex>
+  );
 }
 
 export default CaptionCarousel;
--- src/components/InternProjects/InternProjects.js
+++ src/components/InternProjects/InternProjects.js
@@ -1,81 +1,81 @@
 import { Spacer, VStack } from "@chakra-ui/react";
 import CaptionCarousel from "./Carousel";
 import InternBox from "./InternButton";
-import Image1 from "./../../assets_folder/card2.jpg"
-import Image2 from "./../../assets_folder/card3.jpg"
-import Image1v2 from "./../../assets_folder/InternProjectsImages/Project1.png"
-import Image2v2 from "./../../assets_folder/InternProjectsImages/Project2.png"
+import Image1 from "./../../assets_folder/card2.jpg";
+import Image2 from "./../../assets_folder/card3.jpg";
+import Image1v2 from "./../../assets_folder/InternProjectsImages/Project1.png";
+import Image2v2 from "./../../assets_folder/InternProjectsImages/Project2.png";
 import IntroBanner from "../MembersPage/IntroBanner";
 import InternshipCarousel from "./Carousel2";
-import Intern1 from "./../../assets_folder/InternshipImages/Intern1.jpg"
-import Intern2 from "./../../assets_folder/InternshipImages/intern2v4.png"
-import InternTemp from "./../../assets_folder/InternshipImages/tempIntern2.jpg"
-import Intern3 from "./../../assets_folder/InternshipImages/Intern3v2.png"
+import Intern1 from "./../../assets_folder/InternshipImages/Intern1.jpg";
+import Intern2 from "./../../assets_folder/InternshipImages/Intern2.jpg";
+import InternTemp from "./../../assets_folder/InternshipImages/tempIntern2.jpg";
+import Intern3 from "./../../assets_folder/InternshipImages/Intern3v2.png";
 
 const slidesData = [
-    {
-        image: Image1v2,
-        name: "James Davin",
-        description: "A personal static website",
-        extendedDescription: "A personal static website featuring a Markdown-powered blog tool and an image gallery showcasing my personal portfolio of photographs.",
-        technologies: "Next.js, Typescript, Cloudinary",
-        challenges: "Learning to accept imperfect solutions for smaller problems so that I could solve more problems overall and create a better final product was a skill that I had to develop as development dragged on. In three words? Learning to compromise.",
-        time: "Approximately ~30-50 hours",
-
-    },
-    {
-        image: Image2v2,
-        name: "Andrew Pio George, James Davin and Walker",
-        description: "Stock sentiment analysis tool",
-        extendedDescription: "A tool for any given NASDAQ/NYSE stock that webscrapes the most relevant news articles, passing them through a financial NLP model and returning a semantic analysis with various supplemental quantative metrics.",
-        technologies: "React, Django, Numpy, Pytorch, Axios, BeautifulSoup",
-        challenges: "Development and integration of the three moving parts - the React frontend, Django backend and the NLP model - in the strict time constraint of a 24-hour hackathon proved to be challenging.",
-        time: "72 hours",
-    }
+  {
+    image: Image1v2,
+    name: "James Davin",
+    description: "A personal static website",
+    extendedDescription:
+      "A personal static website featuring a Markdown-powered blog tool and an image gallery showcasing my personal portfolio of photographs.",
+    technologies: "Next.js, Typescript, Cloudinary",
+    challenges:
+      "Learning to accept imperfect solutions for smaller problems so that I could solve more problems overall and create a better final product was a skill that I had to develop as development dragged on. In three words? Learning to compromise.",
+    time: "Approximately ~30-50 hours",
+  },
+  {
+    image: Image2v2,
+    name: "Andrew Pio George, James Davin and Walker",
+    description: "Stock sentiment analysis tool",
+    extendedDescription:
+      "A tool for any given NASDAQ/NYSE stock that webscrapes the most relevant news articles, passing them through a financial NLP model and returning a semantic analysis with various supplemental quantative metrics.",
+    technologies: "React, Django, Numpy, Pytorch, Axios, BeautifulSoup",
+    challenges:
+      "Development and integration of the three moving parts - the React frontend, Django backend and the NLP model - in the strict time constraint of a 24-hour hackathon proved to be challenging.",
+    time: "72 hours",
+  },
 ];
 
-const internsData =  [
-    {
+const internsData = [
+  {
     image: Intern1,
     name: "Aiden Clark",
-    major:"CAS 2025",
+    major: "CAS 2025",
     description: "BI & Analysis Intern",
     company: "The Clearing House",
-    },
+  },
 
-    {
+  {
     image: Intern2,
     name: "Sai Nayunipati",
     major: "C.S. + Stats '25",
     description: "SWE Intern",
-    company: "Jacobs"
-    },
+    company: "Jacobs",
+  },
 
-    {
-        image: Intern3,
-        name: "Grace Elias",
-        major: "CS 2024",
-        description: "SWE Intern",
-        company: "Epic",
-        },
+  {
+    image: Intern3,
+    name: "Grace Elias",
+    major: "CS 2024",
+    description: "SWE Intern",
+    company: "Epic",
+  },
+];
 
+function InternProjects() {
+  return (
+    <VStack spacing={{ base: "1rem", md: "3rem" }} paddingTop="2em">
+      <VStack spacing={{ base: "0.5rem", md: "1.5rem" }}>
+        <IntroBanner content="See Some of Our Highlighted Projects Below" />
+        <CaptionCarousel slides={slidesData} />
+      </VStack>
 
-]
-
-function InternProjects(){
-    return(
-        <VStack spacing={{"base":"1rem","md":"3rem"}} paddingTop="2em">
-            <VStack spacing={{"base":"0.5rem","md":"1.5rem"}}>
-                <IntroBanner content="See Some of Our Highlighted Projects Below" />
-                <CaptionCarousel slides={slidesData}/>
-            </VStack>
-
-            <VStack spacing={{"base":"0.5rem","md":"1.5rem"}}>
-                <IntroBanner content="See the success of UPE Members in Industry!"/>
-                <InternshipCarousel slides={internsData}/>
-            </VStack>
-
-        </VStack>
-    )
+      <VStack spacing={{ base: "0.5rem", md: "1.5rem" }}>
+        <IntroBanner content="See the success of UPE Members in Industry!" />
+        <InternshipCarousel slides={internsData} />
+      </VStack>
+    </VStack>
+  );
 }
-export default InternProjects
+export default InternProjects;

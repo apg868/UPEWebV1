ae28f97951c2374b70665d897ce58b58e086baca
Sean McCarty <soxfansean@gmail.com>
2024-03-01 18:37:56 -0500

    Create studentModel to model mongoDB documents representing students

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const dbURL = process.env.dbURL;
+const port = process.env.port;
 
 app.use(express.json());
 
---
+++ backend/models/studentModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const studentSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        major: {
+            type: String,
+            required: true
+        },
+        year: {
+            type: Number,
+            required: true
+        },
+        funfact: {
+            type: String,
+            required: false
+        },
+        imagePath: {
+            type: String,
+            required: false
+        }
+    }
+);
+
+export const Student = mongoose.model("Student", studentSchema);

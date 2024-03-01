48e701793b096f42e6270b0498c18ca8813c7130
Sean McCarty <soxfansean@gmail.com>
2024-03-01 18:11:25 -0500

    Setup basic index.js structure & add cors

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+//TODO: create db & read port & dbURL from .env file
+mongoose.connect(dbURL)
+    .then(() => {
+        console.log("Successfully connected to db");
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        });
+    });

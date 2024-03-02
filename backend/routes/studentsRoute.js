a7daf8f352d3a0206cff241eb892189a26c357f3
Sean McCarty <soxfansean@gmail.com>
2024-03-02 16:33:57 -0500

    Create get routes, one to get all documents & one for getting by id

--- backend/routes/studentsRoute.js
+++ backend/routes/studentsRoute.js
@@ -36,4 +36,30 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.get("/", async (req, res) => {
+    try {
+        const students = await Student.find({});
+        return res.status(200).json({
+            num: students.length,
+            data: students
+        });
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const student = await Student.findById(id);
+
+        return res.status(200).json(student);
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
 export default router;
